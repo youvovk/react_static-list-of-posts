9b9eb1768fe60e1daf55ae8a1de0188b24d049b5
youvovk <wer3wollf@gmail.com>
2019-09-10 13:38:49 +0300

    Updates

---
+++ precache-manifest.76d06342f835ef432e34c0139ea47135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c4e531f13aa416943bd467769f4f1e",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "0e1d2cb48c5176e59e43",
+    "url": "/react_static-list-of-posts/static/css/main.4466c97a.chunk.css"
+  },
+  {
+    "revision": "cf51463b2ee1efbb8a8f",
+    "url": "/react_static-list-of-posts/static/js/2.48720be0.chunk.js"
+  },
+  {
+    "revision": "0e1d2cb48c5176e59e43",
+    "url": "/react_static-list-of-posts/static/js/main.cc561ea4.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
